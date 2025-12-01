64a46f87048484be904dea00b80faf18e6ce3369
Sayed Abdul-Aziz <sayed.abdulaziz@swapegypt.app>
2025-12-01 16:57:14 +0200

    dart 1

---
+++ dart/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  // int, double , num
+  // round, ceil, floor
+  // toStringAsFixed
+  // toString, toInt , toDouble
+  // int.parse, double.parse
+  // length (1) , index (0)
+  // isEmpty, isNotEmpty
+  // contains, startsWith, endsWith
+
+  //        sayed@       gmail.com
+
+  String name = 'ahmed'; // use toUpperCase and substring
+
+  // Ahmed
+}
